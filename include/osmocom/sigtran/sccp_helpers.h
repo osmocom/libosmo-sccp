e734655afb811ec5a7844beff5c6e9a7ba163683
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2016-03-30 12:44:21 +0200

    sccp_helpers: add convenience function for RANAP unitdata
    Change-Id: Ie96d78512b8e3907753272d75471b882365968c4

--- include/osmocom/sigtran/sccp_helpers.h
+++ include/osmocom/sigtran/sccp_helpers.h
@@ -17,6 +17,11 @@ int osmo_sccp_tx_unitdata_msg(struct osmo_sccp_link *link,
 void osmo_sccp_make_addr_pc_ssn(struct osmo_sccp_addr *addr,
 				uint32_t pc, uint32_t ssn);
 
+int osmo_sccp_tx_unitdata_ranap(struct osmo_sccp_link *link,
+				uint32_t src_point_code,
+				uint32_t dst_point_code,
+				uint8_t *data, unsigned int len);
+
 int osmo_sccp_tx_conn_req(struct osmo_sccp_link *link, uint32_t conn_id,
 			  const struct osmo_sccp_addr *calling_addr,
 			  const struct osmo_sccp_addr *called_addr,
