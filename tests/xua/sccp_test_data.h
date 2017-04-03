192cf0c2ace9316d9ca05e30494b9de6bcacb08c
Harald Welte <laforge@gnumonks.org>
2017-04-03 18:06:50 +0200

    Add tests for xUA code + SCCP/SUA transcoding
    Change-Id: I7ce038d72dca18fb83d5a12519c9a48267e52ab8

---
+++ tests/xua/sccp_test_data.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <stdint.h>
+
+extern const uint8_t bssmap_reset[18];
+extern const uint8_t bssmap_reset_ack[19];
+extern const uint8_t bssmap_paging[32];
+extern const uint8_t bssmap_udt[28];
+extern const uint8_t bssmap_cr[44];
+extern const uint8_t bssmap_cc[10];
+extern const uint8_t bssmap_dtap[22];
+extern const uint8_t bssmap_clear[13];
+extern const uint8_t bssmap_released[14];
+extern const uint8_t bssmap_release_complete[7];
+extern const uint8_t tcap_global_title[183];
