b650dd9d77779b745bd062ed73ff7b7c6d2c9c16
Harald Welte <laforge@gnumonks.org>
2017-04-11 18:36:44 +0200

    osmo_ss7: Use proper string name for dynamically-created ASP
    Change-Id: Id346002c79ba2aba2183ebd46bead372a727316d

--- src/osmo_ss7.c
+++ src/osmo_ss7.c
@@ -1387,7 +1387,7 @@ static int xua_accept_cb(struct osmo_stream_srv_link *link, int fd)
 			char namebuf[32];
 			static uint32_t dyn_asp_num = 0;
 			snprintf(namebuf, sizeof(namebuf), "asp-dyn-%u", dyn_asp_num++);
-			asp = osmo_ss7_asp_find_or_create(oxs->inst, NULL, 0, 0,
+			asp = osmo_ss7_asp_find_or_create(oxs->inst, namebuf, 0, 0,
 							  OSMO_SS7_ASP_PROT_M3UA);
 			if (asp)
 				LOGP(DLSS7, LOGL_INFO, "%s: created dynamicASP %s\n",
