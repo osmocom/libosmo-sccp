3137be99ef2e75bd5bdd616a6c435513a64125ec
Pau Espin Pedrol <pespin@sysmocom.de>
2018-02-20 20:20:10 +0100

    contrib: jenkins.sh: Disable doxygen in libosmocore build
    Change-Id: I7abc8862a63d448408ae43802da689fe436a0ff0

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -23,7 +23,7 @@ verify_value_string_arrays_are_terminated.py $(find . -name "*.[hc]")
 export PKG_CONFIG_PATH="$inst/lib/pkgconfig:$PKG_CONFIG_PATH"
 export LD_LIBRARY_PATH="$inst/lib"
 
-osmo-build-dep.sh libosmocore
+osmo-build-dep.sh libosmocore "" --disable-doxygen
 osmo-build-dep.sh libosmo-abis
 osmo-build-dep.sh libosmo-netif
 
