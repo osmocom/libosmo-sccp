d6cc1294e2e5a1dac2d8a53a907132bc5cc92c0d
Harald Welte <laforge@gnumonks.org>
2019-05-05 16:32:48 +0200

    stp: Ensure telnet uses STP talloc context, not NULL context
    Change-Id: I38a9538dc93cdcecbeb236f0eadc3d675cf20b5d

--- stp/stp_main.c
+++ stp/stp_main.c
@@ -190,7 +190,7 @@ int main(int argc, char **argv)
 
 	osmo_ss7_bind_all_instances();
 
-	rc = telnet_init_dynif(NULL, NULL, vty_get_bind_addr(), OSMO_VTY_PORT_STP);
+	rc = telnet_init_dynif(tall_stp_ctx, NULL, vty_get_bind_addr(), OSMO_VTY_PORT_STP);
 	if (rc < 0) {
 		perror("Error binding VTY port\n");
 		exit(1);
