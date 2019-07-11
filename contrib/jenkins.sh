osmocom/libosmo-sccp

commit 55f03b898a3113f7b73a3abd1c2dfe2edd6135ac

contrib/jenkins.sh: run "make maintainer-clean"
Related: OS#3047
Change-Id: Ia6ebe5add2c1ce6891d596fb2987e46f8ff8a6d6

contrib/jenkins.sh
@@ -54,6 +54,7 @@ $MAKE $PARALLEL_MAKE
 DISTCHECK_CONFIGURE_FLAGS="--enable-external-tests $CONFIG" \
   $MAKE distcheck \
   || cat-testlogs.sh
+$MAKE maintainer-clean
 
 if [ "$WITH_MANUALS" = "1" ] && [ "$PUBLISH" = "1" ]; then
 	make -C "$base/doc/manuals" publish

commit fdff85b8d58eeae4f3216c507c5763a1b452d1d1

contrib/jenkins.sh: "maintainer-clean" after "publish"
Run "make maintainer-clean" after publishing manuals, not the other way
around. Otherwise jenkins.sh fails when running for the master branch,
because docs/manuals/Makefile gets deleted although it is still needed
to publish the manuals.

Related: OS#3047
Fixes: 55f03b898a3113f7b73a3abd1c2dfe2edd6135ac ("contrib/jenkins.sh: run "make maintainer-clean"")
Change-Id: I8bcee9069743b76966a78e1c13d0be9ba62d992c

contrib/jenkins.sh
@@ -54,10 +54,10 @@ $MAKE $PARALLEL_MAKE
 DISTCHECK_CONFIGURE_FLAGS="--enable-external-tests $CONFIG" \
   $MAKE distcheck \
   || cat-testlogs.sh
-$MAKE maintainer-clean
 
 if [ "$WITH_MANUALS" = "1" ] && [ "$PUBLISH" = "1" ]; then
 	make -C "$base/doc/manuals" publish
 fi
 
+$MAKE maintainer-clean
 osmo-clean-workspace.sh