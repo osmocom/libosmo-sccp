3884eb68d9322967340d4f04e7382b2147ec7a2e
Alexander Couzens <lynxis@fe80.eu>
2019-08-06 13:45:03 +0200

    osmo_sccp_simple_client(): use sccp instance index 0 instead of 1
    When using osmo_sccp_simple_client(), it will create an sccp instance if
    none exists. The sccp instance identifier starts with 0.
    The implicit created instance should use sccp instance 0 (the
    first connection).
    
    Change-Id: I9d9f65555b9cdc1e3c697c8b834528d51878e1ae

--- src/sccp_user.c
+++ src/sccp_user.c
@@ -635,7 +635,7 @@ osmo_sccp_simple_client(void *ctx, const char *name, uint32_t default_pc,
 	 *  the only difference is that the ID of the CS7 instance will be
 	 *  set to 1 statically */
 
-	return osmo_sccp_simple_client_on_ss7_id(ctx, 1, name, default_pc, prot,
+	return osmo_sccp_simple_client_on_ss7_id(ctx, 0, name, default_pc, prot,
 						 default_local_port,
 						 default_local_ip,
 						 default_remote_port,
