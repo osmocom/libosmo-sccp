osmocom/libosmo-sccp

commit 3884eb68d9322967340d4f04e7382b2147ec7a2e

osmo_sccp_simple_client(): use sccp instance index 0 instead of 1
When using osmo_sccp_simple_client(), it will create an sccp instance if
none exists. The sccp instance identifier starts with 0.
The implicit created instance should use sccp instance 0 (the
first connection).

Change-Id: I9d9f65555b9cdc1e3c697c8b834528d51878e1ae

src/sccp_user.c
@@ -635,7 +635,7 @@ osmo_sccp_simple_client(void *ctx, const char *name, uint32_t default_pc,
 	 *  the only difference is that the ID of the CS7 instance will be
 	 *  set to 1 statically */
 
-	return osmo_sccp_simple_client_on_ss7_id(ctx, 1, name, default_pc, prot,
+	return osmo_sccp_simple_client_on_ss7_id(ctx, 0, name, default_pc, prot,
 						 default_local_port,
 						 default_local_ip,
 						 default_remote_port,

commit 87c1550d7056a711317b04af184292ed7c7c86a8

sccp_user: correct comment for osmo_sccp_simple_client
Fixes: 3884eb68d932 ("osmo_sccp_simple_client(): use sccp instance index 0 instead of 1")
Change-Id: Iaac59222df84eacf813825c2ef660281381e6b14

src/sccp_user.c
@@ -633,7 +633,7 @@ osmo_sccp_simple_client(void *ctx, const char *name, uint32_t default_pc,
 {
 	/*! This is simplified version of osmo_sccp_simple_client_on_ss7_id().
 	 *  the only difference is that the ID of the CS7 instance will be
-	 *  set to 1 statically */
+	 *  set to 0 statically */
 
 	return osmo_sccp_simple_client_on_ss7_id(ctx, 0, name, default_pc, prot,
 						 default_local_port,