e7c8067df026fd294af2c95fbd2ceb16eeab7d74
Pau Espin Pedrol <pespin@sysmocom.de>
2020-10-12 18:28:55 +0200

    contrib/jenkins: Enable parallel make in make distcheck
    Change-Id: I667b245e468b9a2bcc5c9a1a04d59a940e71b24c
    Related: OS#4421

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -52,12 +52,12 @@ autoreconf --install --force
 ./configure --enable-sanitize --enable-werror --enable-external-tests $CONFIG
 $MAKE $PARALLEL_MAKE
 DISTCHECK_CONFIGURE_FLAGS="--enable-external-tests $CONFIG" \
-  $MAKE distcheck \
+  $MAKE $PARALLEL_MAKE distcheck \
   || cat-testlogs.sh
 
 if [ "$WITH_MANUALS" = "1" ] && [ "$PUBLISH" = "1" ]; then
 	make -C "$base/doc/manuals" publish
 fi
 
-$MAKE maintainer-clean
+$MAKE $PARALLEL_MAKE maintainer-clean
 osmo-clean-workspace.sh
