1e2ff4a8b87e37c830658a2b48e60e8eac2499f7
Oliver Smith <osmith@sysmocom.de>
2021-01-13 13:10:28 +0100

    contrib/jenkins: don't build osmo-gsm-manuals
    Related: OS#4912
    Change-Id: I511b2e1f4c3a9e0897cff4241ab5df12327de10d

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -36,7 +36,6 @@ osmo-build-dep.sh libosmo-netif
 # Additional configure options and depends
 CONFIG=""
 if [ "$WITH_MANUALS" = "1" ]; then
-	osmo-build-dep.sh osmo-gsm-manuals
 	CONFIG="--enable-manuals"
 fi
 
