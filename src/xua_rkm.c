1a75755f9b47bc93600dafd61c5ea1e1ad283d68
Oliver Smith <osmith@sysmocom.de>
2021-07-23 09:45:11 +0200

    m3ua: reject deregistration of static routing keys
    Make STP_Tests_M3UA.TC_rkm_unreg_never_registered pass.
    
    Closes: OS#4239
    Change-Id: Ie5d84d06e7d5d32c8a22f770f71a5449f4a78484

--- src/xua_rkm.c
+++ src/xua_rkm.c
@@ -350,6 +350,12 @@ static int handle_rkey_dereg(struct osmo_ss7_asp *asp, uint32_t rctx,
 		return -1;
 	}
 
+	/* Reject if not dynamically allocated (OS#4239) */
+	if (!as->rkm_dyn_allocated) {
+		msgb_append_dereg_res(resp, M3UA_RKM_DEREG_ERR_NOT_REGD, 0);
+		return -1;
+	}
+
 	/* Reject if ASP is not even part of AS */
 	if (!osmo_ss7_as_has_asp(as, asp)) {
 		msgb_append_dereg_res(resp, M3UA_RKM_DEREG_ERR_INVAL_RCTX, 0);
