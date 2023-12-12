osmocom/libosmo-sccp

commit 65741dca056e3a16973ad156dd4c09760a6a945b

ipa: Fix client setting unit_id with ASP name instead of AS
This was broken since ever. The client was submitting the ASP name
 in the unit_id field during IPA handshake, and the server was
expecting it to contain the AS, so it failed with message:
"Cannot find any definition for IPA Unit Name '%s'" in
ipa_asp_fsm_wait_id_resp().

Fixes: 5f0a8df34cae20455a520c3a85cf94124f486bf2
Change-Id: I249964e171f578726439c40e01ae85aa447afada

src/xua_asp_fsm.c
@@ -1190,9 +1190,15 @@ static struct osmo_fsm_inst *ipa_asp_fsm_start(struct osmo_ss7_asp *asp,
 {
 	struct osmo_fsm_inst *fi;
 	struct ipa_asp_fsm_priv *iafp;
+	struct osmo_ss7_as *as = ipa_find_as_for_asp(asp);
 
 	/* allocate as child of AS? */
 	fi = osmo_fsm_inst_alloc(&ipa_asp_fsm, asp, NULL, log_level, asp->cfg.name);
+
+	if (!as) {
+		osmo_fsm_inst_term(fi, OSMO_FSM_TERM_ERROR, NULL);
+		return NULL;
+	}
 
 	iafp = talloc_zero(fi, struct ipa_asp_fsm_priv);
 	if (!iafp) {
@@ -1202,7 +1208,7 @@ static struct osmo_fsm_inst *ipa_asp_fsm_start(struct osmo_ss7_asp *asp,
 	iafp->role = role;
 	iafp->asp = asp;
 	iafp->ipa_unit = talloc_zero(iafp, struct ipaccess_unit);
-	iafp->ipa_unit->unit_name = talloc_strdup(iafp->ipa_unit, asp->cfg.name);
+	iafp->ipa_unit->unit_name = talloc_strdup(iafp->ipa_unit, as->cfg.name);
 	iafp->pong_timer.cb = ipa_pong_timer_cb;
 	iafp->pong_timer.data = fi;
 

src/ipa.c
@@ -131,7 +131,7 @@ static int ipa_rx_msg_ccm(struct osmo_ss7_asp *asp, struct msgb *msg)
 	return 0;
 }
 
-static struct osmo_ss7_as *find_as_for_asp(struct osmo_ss7_asp *asp)
+struct osmo_ss7_as *ipa_find_as_for_asp(struct osmo_ss7_asp *asp)
 {
 	struct osmo_ss7_as *as;
 
@@ -216,7 +216,7 @@ static int ipa_rx_msg_sccp(struct osmo_ss7_asp *asp, struct msgb *msg, uint8_t s
 	int rc;
 	struct m3ua_data_hdr data_hdr;
 	struct xua_msg *xua;
-	struct osmo_ss7_as *as = find_as_for_asp(asp);
+	struct osmo_ss7_as *as = ipa_find_as_for_asp(asp);
 	uint32_t opc, dpc;
 
 	if (!as) {

src/xua_internal.h
@@ -117,6 +117,7 @@ int xua_find_as_for_asp(struct osmo_ss7_as **as, const struct osmo_ss7_asp *asp,
 
 int ipa_tx_xua_as(struct osmo_ss7_as *as, struct xua_msg *xua);
 int ipa_rx_msg(struct osmo_ss7_asp *asp, struct msgb *msg, uint8_t sls);
+struct osmo_ss7_as *ipa_find_as_for_asp(struct osmo_ss7_asp *asp);
 
 int osmo_isup_party_parse(char *out_digits, const uint8_t *in,
 			unsigned int in_num_bytes, bool odd);

commit 7aee5de459f01223909334206a16e051e1c364d2

ipa: Use ASP name as ipa_unit_name on dynamic ASPs
A recent patch fixed a long problem where the ASP name (instead of
expected AS name) was used as ipa_unit_name in IPA based ASPs.
For server side it doesn't matter much, sense anyway the ipa_unit_name
is actually restored on the struct with what's received in IPA GET_RESP
message (see ipa_asp_fsm_wait_id_resp()). So the fix was actually for
the client side in the scenario where a non-dynamic ASP with an assigned
AS was configured in the VTY.

However, dynamic ASPs usually have no assigned AS (because in general it
is really not created/configured, as the ASP is created on the flight).
As a result, the recent patch (see "Fixes" below), broke dynamic ASPs
case because from then one ipa_asp_fsm_start() would fail and terminate
the FSM because ipa_find_as_for_asp() was returning NULL.

So improve the recent patch by applying the previous logic for dynamic
ASPs:
* On the server side, it really doesn't matter since as mentioned, the
  field will be repopulated later on, but allows the code to avoid
terminating the FSM.
* On the client case, this is how dynamic IPA ASPs were mento be used
  when they were introduced anyway.

Fixes: 65741dca056e3a16973ad156dd4c09760a6a945b
Change-Id: I0a741449450c998253b1e44a76a3b7fc224e0903
Related: SYS#5914

src/xua_asp_fsm.c
@@ -1191,24 +1191,33 @@ static struct osmo_fsm_inst *ipa_asp_fsm_start(struct osmo_ss7_asp *asp,
 	struct osmo_fsm_inst *fi;
 	struct ipa_asp_fsm_priv *iafp;
 	struct osmo_ss7_as *as = ipa_find_as_for_asp(asp);
+	const char *unit_name;
 
 	/* allocate as child of AS? */
 	fi = osmo_fsm_inst_alloc(&ipa_asp_fsm, asp, NULL, log_level, asp->cfg.name);
-
-	if (!as) {
-		osmo_fsm_inst_term(fi, OSMO_FSM_TERM_ERROR, NULL);
-		return NULL;
-	}
 
 	iafp = talloc_zero(fi, struct ipa_asp_fsm_priv);
 	if (!iafp) {
 		osmo_fsm_inst_term(fi, OSMO_FSM_TERM_ERROR, NULL);
 		return NULL;
 	}
+
+	if (as) {
+		unit_name = as->cfg.name;
+	} else if (asp->dyn_allocated) {
+		LOGPFSML(fi, LOGL_INFO, "Dynamic ASP is not assigned to any AS, "
+			 "using ASP name instead of AS name as ipa_unit_name\n");
+		unit_name = asp->cfg.name;
+	} else {
+		LOGPFSML(fi, LOGL_ERROR, "ASP is not assigned to any AS, fix your config!\n");
+		osmo_fsm_inst_term(fi, OSMO_FSM_TERM_ERROR, NULL);
+		return NULL;
+	}
+
 	iafp->role = role;
 	iafp->asp = asp;
 	iafp->ipa_unit = talloc_zero(iafp, struct ipaccess_unit);
-	iafp->ipa_unit->unit_name = talloc_strdup(iafp->ipa_unit, as->cfg.name);
+	iafp->ipa_unit->unit_name = talloc_strdup(iafp->ipa_unit, unit_name);
 	iafp->pong_timer.cb = ipa_pong_timer_cb;
 	iafp->pong_timer.data = fi;
 