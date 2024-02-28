6b95322bd6f1e90cb57c8d66657922eb04db2e21
Harald Welte <laforge@osmocom.org>
2024-02-28 19:33:11 +0100

    ipa: Use pseudo-random number for SLS in IPA->M3UA direction
    [back-port of Ia4e66d660b6057338f66a47fffc8a0d32759f733]
    
    In Change-Id Ice7bab997b84cfed00c7d6d780c70f4e9fac6002 we introduced
    code that would make the LSB of the file descriptor be used as SLS
    when passing packets from IPA in M3UA direction.
    
    This did however not achieve sufficient entropy in real-world use cases.
    
    In this change, we change over to allocating a pseudo-random SLS to each
    IPA connection at the time it is established;  We then assign that SLS
    to each packet received on that IPA connection.
    
    Change-Id: I97f43bd3bdd89a6bf21a84284a060305fcbb86e7
    Related: SYS#6543
    Closes: SYS#6802

--- stp/stp_main.c
+++ stp/stp_main.c
@@ -213,6 +213,8 @@ int main(int argc, char **argv)
 {
 	int rc;
 
+	srand(time(NULL));
+
 	tall_stp_ctx = talloc_named_const(NULL, 1, "osmo-stp");
 	msgb_talloc_ctx_init(tall_stp_ctx, 0);
 	osmo_init_logging2(tall_stp_ctx, &log_info);
